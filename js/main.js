5023bf521a29957cd55c427ab8b03d5e4662a4c8
batzner <github@kilians.net>
2017-09-06 23:12:48 +0200

    Implemented basic game logic; Switched to grid as background image

--- js/main.js
+++ js/main.js
@@ -2,65 +2,108 @@
  * Created by Kilian on 06.09.17.
  */
 
-const CANVAS_PADDING = 2;
-const FIELD = $("#field");
-const FIELD_CONTAINER = $("#field-container");
+const CELL_WIDTH = 12;
+const CELL_HEIGHT = 12;
+const NUM_ROWS = 50;
+const NUM_COLUMNS = 100;
 
-let cellWidth = 0;
-let cellHeight = 0;
+const FIELD = getField(NUM_ROWS, NUM_COLUMNS);
 
+let STAGE = null;
+let SHAPES = null;
 
 $(function () {
-    drawGrid(100, 50);
+    // Initialize the semi-constants
+    STAGE = new createjs.Stage("canvas");
+    SHAPES = getShapes(STAGE, NUM_ROWS, NUM_COLUMNS);
 
-    // Draw a square on screen.
-    const stage = new createjs.Stage("canvas");
-    const shape = new createjs.Shape();
-    shape.graphics.beginFill('rgba(0,255,0,0.2)').drawRect(0, 0, 120, 120);
-    stage.addChild(shape);
-    stage.update();
-});
-
-function drawGrid(columns, rows) {
-    updateField(columns / rows);
-
-    const rough = new RoughCanvas(document.getElementById('grid-canvas'));
-
-    // Determine the cell size from the canvas size
-    cellWidth = Math.floor((rough.width - 2 * CANVAS_PADDING) / columns);
-    cellHeight = Math.floor((rough.height - 2 * CANVAS_PADDING) / rows);
-
-    // Make the cells square
-    cellWidth = Math.min(cellHeight, cellWidth);
-    //noinspection JSSuspiciousNameCombination
-    cellHeight = cellWidth;
-
-    const strokeWidth = cellHeight * 0.5 / 40;
-
-    for (let rowIndex = 0; rowIndex < rows; rowIndex++) {
-        for (let columnIndex = 0; columnIndex < columns; columnIndex++) {
-            const rect = rough.rectangle(
-                CANVAS_PADDING + columnIndex * cellWidth,
-                CANVAS_PADDING + rowIndex * cellHeight,
-                cellWidth, cellHeight);
-            rect.fill = "rgba(0,0,0,0)";
-            rough.strokeWidth = strokeWidth;
-        }
-    }
-}
-
-function updateField(widthHeightRatio) {
-    // Try to make the FIELD as large as possible
-    let maxHeight = FIELD_CONTAINER.height();
-    let maxWidth = FIELD_CONTAINER.width();
-
-    FIELD.width(Math.min(maxWidth, widthHeightRatio * maxHeight));
-    FIELD.height(Math.min(maxHeight, maxWidth / widthHeightRatio));
-
-    // Let the canvasses fill the FIELD
+    // Let the canvas fill the screen
     $("canvas").each(function (_, el) {
         const canvas = el.getContext("2d").canvas;
-        canvas.width = FIELD.width();
-        canvas.height = FIELD.height();
+        canvas.width = $(window).width();
+        canvas.height = $(window).height();
     });
+
+    randomInit();
+    drawField();
+    createjs.Ticker.addEventListener("tick", tick);
+    createjs.Ticker.setFPS(20);
+});
+
+function tick() {
+    // Update the field and draw it
+    for (let row =0; row < FIELD.length; row++) {
+        for (let column = 0; column < FIELD[row].length; column++) {
+            const neighborsCount = getNeighborsCount(row, column);
+            FIELD[row][column] = neighborsCount == 3 || neighborsCount == 2 && FIELD[row][column];
+        }
+    }
+
+    drawField();
+}
+
+function getNeighborsCount(cellRow, cellColumn) {
+    const rowStart = Math.max(cellRow-1, 0);
+    const rowEnd = Math.min(cellRow+2, NUM_ROWS);
+    const columnStart = Math.max(cellColumn-1, 0);
+    const columnEnd = Math.min(cellColumn+2, NUM_COLUMNS);
+
+    let neighborsCount = 0;
+
+    for (let row = rowStart; row < rowEnd; row++) {
+        for (let column = columnStart; column < columnEnd; column++) {
+            if (FIELD[row][column] == true && !(row == cellRow && column == cellColumn)) {
+                neighborsCount += 1;
+            }
+        }
+    }
+    return neighborsCount;
+}
+
+function drawField() {
+    for (let row =0; row < FIELD.length; row++) {
+        for (let column = 0; column < FIELD[row].length; column++) {
+            SHAPES[row][column].visible = FIELD[row][column] == true;
+        }
+    }
+    STAGE.update();
+}
+
+function getField(rows, columns) {
+    let field = [];
+    for (let row = 0; row < rows; row++) {
+        let entriesRow = [];
+        for (let column = 0; column < columns; column++) {
+            entriesRow.push(false);
+        }
+        field.push(entriesRow);
+    }
+    return field;
+}
+
+function getShapes(stage, rows, columns) {
+    let shapes = [];
+    for (let row = 0; row < rows; row++) {
+        let shapesRow = [];
+        for (let column = 0; column < columns; column++) {
+            // Create the shape
+            const shape = new createjs.Shape();
+            shape.graphics
+                .beginFill('rgba(255,0,0,0.2)')
+                .drawRect(column * CELL_WIDTH, row * CELL_HEIGHT, CELL_WIDTH, CELL_HEIGHT);
+            stage.addChild(shape);
+
+            shapesRow.push(shape);
+        }
+        shapes.push(shapesRow);
+    }
+    return shapes;
+}
+
+function randomInit() {
+    for (let row =0; row < FIELD.length; row++) {
+        for (let column = 0; column < FIELD[row].length; column++) {
+            FIELD[row][column] = Math.random() < 0.1;
+        }
+    }
 }
