ec8ff122e063254fdb962a2383dec53f912e73a3
Kilian Batzner <git@kilians.net>
2019-11-24 23:30:02 +0100

    Add html and css minification

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,9 @@ const clean = require('gulp-clean');
 const babel = require('gulp-babel');
 const uglify = require('gulp-uglify');
 const imagemin = require('gulp-imagemin');
-const rename = require('gulp-rename');
+const htmlmin = require('gulp-htmlmin');
+const minifyCSS = require('gulp-minify-css');
+const minifyInline = require('gulp-minify-inline');
 
 // Base for specifying paths in gulp.src and gulp.dest
 const distDir = 'dist/';
@@ -15,8 +17,9 @@ const srcDir = 'src/';
 // Paths to be used in gulp.src
 const sourcePaths = {
     javascripts: ['js/**/*.js', '!js/lib/**/*.js'],
-    stylesheets: ['css/**/*.css'],
-    images: ['img/**/*.png', 'img/**/*.jpg', 'img/**/*.jpeg', 'img/**/*.gif']
+    images: ['img/**/*.png', 'img/**/*.jpg', 'img/**/*.jpeg', 'img/**/*.gif'],
+    html: ['**/*.html'],
+    css: ['css/**/*.css']
 };
 
 // Delete the dist directory
@@ -47,8 +50,26 @@ gulp.task('_images', ['_copy'], function () {
         .pipe(gulp.dest(distDir));
 });
 
+// Minify html
+gulp.task('_html', ['_copy'], function () {
+    return gulp.src(sourcePaths.html, {base: distDir, cwd: distDir})
+        .pipe(htmlmin({
+            collapseWhitespace: true,
+            removeComments: true
+        }))
+        .pipe(minifyInline())
+        .pipe(gulp.dest(distDir));
+});
+
+// Minify css
+gulp.task('_css', ['_copy'], function () {
+    return gulp.src(sourcePaths.css, {base: distDir, cwd: distDir})
+        .pipe(minifyCSS())
+        .pipe(gulp.dest(distDir));
+});
+
 // Default task producing a ready-to-ship frontend in the dist folder
-gulp.task('default', ['_javascripts', '_images']);
+gulp.task('default', ['_copy', '_javascripts', '_images', '_html', '_css']);
 
 // Check code style on JS
 gulp.task('eslint', function () {
